488973b84109030887225258102264a0d66c5a98
yuna-Song-yuna <68729868+yuna-Song-yuna@users.noreply.github.com>
2021-01-12 17:46:43 +0900

    Update 모의고사.js

--- 프로그래머스/level1/모의고사.js
+++ 프로그래머스/level1/모의고사.js
@@ -1,52 +1,31 @@
+// 완전탐색
 function solution(answers) {
     var answer = [];
-    var j = 0;
-    var i = 0;
-    var num1 = [1,2,3,4,5]
-    var num2 = [2,1,2,3,2,4,2,5]
-    var num3 = [3,3,1,1,2,2,4,4,5,5]
-    var num1_answer = 0;
-    var num2_answer = 0;
-    var num3_answer = 0;
+    var num1 = [1,2,3,4,5]              //수포자1 패턴
+    var num2 = [2,1,2,3,2,4,2,5]        //수포자2 패턴 
+    var num3 = [3,3,1,1,2,2,4,4,5,5]    //수포자3 패턴
+    var count = [0,0,0]
     
-    for(i=0; i<=answers.length; i++){
-        if(answers[i] == num1[j]){
-            num1_answer += 1;
+    for(var i=0; i<answers.length; i++){    //답 대조 => 맞은 답 개수 카운트
+        if(num1[i%5] == answers[i]){
+            count[0]++;
         }
-        if(j == 5){
-            j = 0;
-        }else{
-            j++
+        if(num2[i%8] == answers[i]){
+             count[1]++;
+         }
+        if(num3[i%10] == answers[i]){
+            count[2]++;
         }
-    //console.log(num1_answer)
     }
     
-   for(i=0; i<=answers.length; i++){
-    if(answers[i] == num2[j]){
-        num2_answer += 1;
-    }
-    if(j == 7){
-        j = 0;
-    }else{
-        j++
-    }
-    console.log(num2_answer)
-    }
-   
-    for(i=0; i<=answers.length; i++){
-    if(answers[i] == num3[j]){
-        num3_answer += 1;
-    }
-    if(j == 9){
-        j = 0;
-    }else{
-        j++
-    }
-    console.log(num3_answer)
-    }
+    var index
+    index = Math.max(count[0], count[1], count[2])  //가장 많이 맞춘 개수 반환
     
+    for(var i=0; i<count.length; i++){      //가장 많이 맞춘 개수가 위치한 배열 번호 반환(=수포자 번호) 후 answer에 push
+        if(count[i] == index){
+            answer.push(i+1)
+        }
+    }
     
     return answer;
 }
-
-//이건 아닌 것 같다
